969273c95be990fca9acb671a5d8c5d24f5e5ec5
nbadalia <nare.badalian@yahoo.com>
2024-09-01 09:26:31 +0400

    01, 02

---
+++ ex02/WrongCat.hpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "WrongAnimal.hpp"
+
+#ifndef WRONGCAT_HPP
+#define WRONGCAT_HPP
+
+class WrongCat : public WrongAnimal
+{
+	public: 
+	WrongCat();
+	~WrongCat();
+	WrongCat(const WrongCat &objs);
+	WrongCat &operator=(const WrongCat &objs);
+	void makeSound() const;
+};
+
+#endif
